b3e0190fb8f3a025ecc852ed920d234c169eab90
haikhanh9484 <hai.khanh.9484@gmail.com>
2021-09-09 17:16:58 +0700

    fix base64 to img

--- src/main/java/com/example/demo/controller/ConvertController.java
+++ src/main/java/com/example/demo/controller/ConvertController.java
@@ -7,10 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -36,45 +33,13 @@ public class ConvertController {
         return dataService.ImgToBase64(multipartFile);
     }
 
-//    @PostMapping("/convert/img/decode")
-//    public Map<String, String> Base64ToImg(@RequestBody Data data) {
-//        File serverFile = new File( new File("src\\main\\resources\\static").getAbsolutePath() + "\\demo.jpg");
-//        if(serverFile.delete())
-//        {
-//            System.out.println("File deleted successfully");
-//        }
-//        else
-//        {
-//            System.out.println("Failed to delete the file");
-//        }
-//
-//        HashMap<String, String> map = new HashMap<>();
-//        if(convertStringToImage(data.getValue())){
-//            map.put("value", "true");
-//            return map;
-//        }else{
-//            map.put("value", "false");
-//            return map;
-//        }
-//    }
-
     @PostMapping("/convert/img/decode")
     public Boolean Base64ToImg(@RequestBody Data data) {
-        File serverFile = new File(new File("src\\main\\resources\\static").getAbsolutePath() + "\\demo.jpg");
-        if (serverFile.delete()) {
-            System.out.println("File deleted successfully");
-        } else {
-            System.out.println("Failed to delete the file");
-        }
-        return dataService.Base64ToImg(data);
+        return dataService.Base64ToByte(data);
     }
 
     @GetMapping(value = "/img", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> GetImg() throws IOException {
-        File serverFile = new File(new File("src\\main\\resources\\static").getAbsolutePath() + "\\demo.jpg");
-
-        byte[] fileContent = Files.readAllBytes(serverFile.toPath());
-        System.out.println("serverFile : " + serverFile);
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(fileContent);
+    public ResponseEntity<byte[]> GetImg(){
+        return dataService.ByteToImage();
     }
 }
--- src/main/java/com/example/demo/service/DataService.java
+++ src/main/java/com/example/demo/service/DataService.java
@@ -1,26 +1,25 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Data;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class DataService {
-
-    public static byte[] decodeImage(String imageDataString) {
-        return org.apache.tomcat.util.codec.binary.Base64.decodeBase64(imageDataString);
-    }
+    private byte[] imageByteArray;
 
     public Map<String, String> TextToBase64(Data data) {
-        data.setValue(Base64.getEncoder().encodeToString(data.getValue().getBytes()));
-//        System.out.println(data.getValue());
+        String base64 = Base64.getEncoder().encodeToString(data.getValue().getBytes());
         HashMap<String, String> map = new HashMap<>();
-        map.put("value", data.getValue());
+        map.put("value", base64);
         return map;
     }
 
@@ -29,7 +28,6 @@ public class DataService {
         try {
             byte[] decodedBytes = Base64.getDecoder().decode(data.getValue());
             String decodedString = new String(decodedBytes);
-//        System.out.println(decodedString);
             map.put("value", decodedString);
             return map;
         } catch (Exception e) {
@@ -38,49 +36,41 @@ public class DataService {
         }
     }
 
-    public Map<String, String> ImgToBase64(MultipartFile multipartFile) throws IOException {
-        File file = new File(multipartFile.getName());
-        try (OutputStream os = new FileOutputStream(file)) {
-            os.write(multipartFile.getBytes());
-        }
-
+    public Map<String, String> ImgToBase64(MultipartFile multipartFile){
         String base64Image = "";
-        try (FileInputStream imageInFile = new FileInputStream(file.getName())) {
-            //Reading a Image file from file system
-            byte imageData[] = new byte[(int) file.length()];
-            imageInFile.read(imageData);
-            base64Image = Base64.getEncoder().encodeToString(imageData);
-        } catch (FileNotFoundException e) {
-            System.out.println("Image not found" + e);
-        } catch (IOException ioe) {
-            System.out.println("Exception while reading the Image " + ioe);
+        try{
+            //convert MultipartFile to File
+            File file = new File(multipartFile.getName());
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write( multipartFile.getBytes() );
+            fos.close();
+            //convert file to base64
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+            base64Image = Base64.getEncoder().encodeToString(fileContent);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
-
         HashMap<String, String> map = new HashMap<>();
         map.put("value", base64Image);
         return map;
     }
 
-    public boolean Base64ToImg(Data data) {
-        if (convertStringToImage(data.getValue())) {
+    public boolean Base64ToByte(Data data) {
+        try {
+            imageByteArray = decodeImage(data.getValue());
             return true;
-        } else {
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            imageByteArray = new byte[]{};
             return false;
         }
     }
 
-    public boolean convertStringToImage(String base64) {
-        try {
-            byte[] imageByteArray = decodeImage(base64);
-            String fileLocation = new File("src\\main\\resources\\static").getAbsolutePath() + "\\demo.jpg";
+    public static byte[] decodeImage(String imageDataString) {
+        return org.apache.tomcat.util.codec.binary.Base64.decodeBase64(imageDataString);
+    }
 
-            FileOutputStream imageOutFile = new FileOutputStream(fileLocation);
-            imageOutFile.write(imageByteArray);
-            imageOutFile.close();
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
+    public ResponseEntity<byte[]> ByteToImage(){
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageByteArray);
     }
 }
